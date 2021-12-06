1ce7e19d2294d8976a74bdec6333cf2c4f4b5e79
Leon Carbonell <leoncarbonell@gmail.com>
2021-12-06 17:54:37 +1030

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Board from './components/Board';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>This is a boarding call</h2>
+      <Board />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Board.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Board() {
+  return (
+    <div>
+      <h2>This is across the board</h2>
+    </div>
+  )
+}
